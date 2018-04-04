036b51d511ec690a10a9b16116aee676828ac50d
hkanezashi <kanezashi.h@gmail.com>
2018-04-04 18:15:28 +0900

    Add a test script for incremental G-Ray

---
+++ test/script.py
@@ -0,0 +1,9 @@
+from patternmatching.gray.incremental import query_call
+
+fname = "./sample/dynamic/test0.json"
+qargs = "--vertex a b c --edge x:a:b y:b:c z:c:a --vertexlabel a:cyan b:cyan c:cyan".split(" ")
+query_call.run_query(fname, qargs, False, False, 2)
+
+fname = "./sample/dynamic/test1.json"
+qargs = "--vertex a b c --edge x:a:b y:b:c z:c:a --vertexlabel a:cyan b:cyan c:cyan".split(" ")
+query_call.run_query(fname, qargs, False, False, 3)
